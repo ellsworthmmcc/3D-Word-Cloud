398ec837af9684177266e4dddd6878eda3e0e445
Ellsworth McCullough <ellsworthmmcc@outlook.com>
2026-02-25 23:46:00 -0800

    refactor: moved files into seperate modules

--- backend/database/database.py
+++ backend/database/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import (
 )
 from sqlalchemy.orm import DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./blog.db"
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./3d-word-cloud.db"
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
--- backend/models/models.py
+++ backend/models/models.py
@@ -5,17 +5,26 @@ from datetime import UTC, datetime
 from sqlalchemy import DateTime, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from database import Base
+from backend.database.database import Base
 
 
 class Article(Base):
     __tablename__ = 'article'
 
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        index=True,
+        nullable=False,
+    )
+
     url: Mapped[str] = mapped_column(
         String(150),
         unique=True,
-        nullable=False,)
+        nullable=False,
+    )
 
     date_created: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
-        default=lambda: datetime.now(UTC),)
+        default=lambda: datetime.now(UTC),
+    )
