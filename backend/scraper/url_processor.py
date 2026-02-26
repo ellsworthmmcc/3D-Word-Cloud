0771df9baf8d0dad3bf6b35c2236263fcbc1739d
Ellsworth McCullough <ellsworthmmcc@outlook.com>
2026-02-26 10:51:22 -0800

    feat: better error handling

--- backend/scraper/url_processor.py
+++ backend/scraper/url_processor.py
@@ -13,7 +13,7 @@ async def url_processor(url: str) -> dict[str, float] | None:
     try:
         processed_words = await processor(words=scraped_words)
     except Exception as e:
-        print(f'Unable to process article, \n{e}')
+        print(f"Unable to process article: {type(e).__name__}: {e}")
         return None
 
     return processed_words
